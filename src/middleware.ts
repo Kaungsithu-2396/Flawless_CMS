f216357fb30caed44682ab7038359bba3833b83a
Kaungsithu-123 <Kaungsithu2396@gmail.com>
2024-10-17 14:02:09 +0700

    add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export function middleware(request: NextRequest, response: NextResponse) {
+    const token = cookies().get("token");
+    console.log(response);
+    const isOnLoginPage = request.nextUrl.pathname === "/";
+    const isOnDashboardPage = request.nextUrl.pathname === "/dashboard";
+
+    if (!token && !isOnLoginPage) {
+        return NextResponse.redirect(new URL("/", request.url));
+    }
+
+    if (token && isOnLoginPage) {
+        return NextResponse.redirect(new URL("/dashboard", request.url));
+    }
+
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        "/",
+        "/category/:path*",
+        "/contact/:path*",
+        "/dashboard/:path*",
+        "/detail/:path*",
+        "/product/:path*",
+    ],
+};
